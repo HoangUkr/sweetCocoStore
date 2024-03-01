fc35650a221753380e376fb161abf28482b6deac
Pham Hoang <pxhoang@eccentex.com>
2024-03-01 17:44:43 +0100

    Fix url, home page and add shop page

--- sweetCocoWeb/urls.py
+++ sweetCocoWeb/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'sweetCocoWeb'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('shop/', views.shop, name='shop')
 ]
--- sweetCocoWeb/views.py
+++ sweetCocoWeb/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 
+def index(request):
+    return redirect(reverse('sweetCocoWeb:home'))
 # Render home page
 def home(request):
-    pass
+    return render(request, "PAGE/index.html")
+
+# Render shop page
+def shop(request):
+    return render(request, 'PAGE/shop.html')
