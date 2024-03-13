dd1bf3dc150f39157b816d24efcccb2b7a211f6a
Pham Hoang <pxhoang@eccentex.com>
2024-03-13 21:37:34 +0100

    Increase quantity button

--- sweetCocoWeb/urls.py
+++ sweetCocoWeb/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('shop/', views.shop, name='shop'),
     path('cart/', views.cart, name='cart'),
-    path('add_product/<int:pk>/', views.add_to_cart_ajax, name='add_product'),
-    path('delete_item_from_cart/<int:order_id>/<int:order_item_id>/', views.delete_item_from_cart, name='delete_item_from_cart'),
-    path('change_quantity/<int:pk>/', views.change_quantity, name='change_quantity'),
+    path('add_product/', views.add_to_cart_ajax, name='add_product'),
+    path('delete_item_from_cart/', views.delete_item_from_cart, name='delete_item_from_cart'),
+    path('change_quantity/', views.change_quantity, name='change_quantity'),
 
     # API URL
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
--- sweetCocoWeb/views.py
+++ sweetCocoWeb/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from .models import Product, Order, OrderItem
 from django.http import JsonResponse, Http404
+from django.views.decorators.http import require_http_methods
+import json
 # Create your views here.
 
 def index(request):
@@ -38,55 +40,117 @@ def update_total(order):
     order.save()
 
 # Add to cart AJAX function
-def add_to_cart_ajax(request, pk):
-    try:
-        product = get_object_or_404(Product, pk=pk)
-        if request.method == 'POST':
+def add_to_cart_ajax(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            product_id = data.get('product_id')
+            product = get_object_or_404(Product, id=product_id)
             recent_order, created = Order.objects.get_or_create(isSent=False)
             order_item = OrderItem.objects.create(order=recent_order, product=product)
             order_item.save()
             update_total(order=recent_order)
-
             response_data = {
                 'message': 'Product added to the cart successfully',
                 'errorcode': 0
             }
-            
-        else:
+            return JsonResponse(response_data)
+        except Http404:
             response_data = {
-                'message': 'Wrong request. Please contact admin',
+                'message': 'Product not found. Please contact admin.',
                 'errorcode': 1
             }
-        return JsonResponse(response_data)
-    except Http404:
+            return JsonResponse(response_data)
+    else:
         response_data = {
-            'message': 'Product not found. Please contact admin.',
+            'message': 'Wrong request. Please contact admin',
             'errorcode': 1
         }
         return JsonResponse(response_data)
     
 # Delete item from order
-def delete_item_from_cart(request, order_item_id, order_id):
-    try:
-        order = get_object_or_404(Order, orderId=order_id)
-        if request.method == 'DELETE':
-            order_items = OrderItem.objects.get(order=order, id=order_item_id)
-            order_items.delete()
+def delete_item_from_cart(request):
+    if request.method == 'DELETE':
+        try:
+            data = json.loads(request.body)
+            order_id = data.get('order_id')
+            order_item_id = data.get('order_item_id')
+            print(f'Order ID: {order_id}. Order Item ID: {order_item_id}.')
+            #Get order and order item
+            order = get_object_or_404(Order, orderId=order_id)
+            order_item = get_object_or_404(OrderItem, id=order_item_id, order=order)
+
+            # Delete record
+            order_item.delete()
+            # Update total price in Order
             update_total(order)
             response_data = {
                 'message': 'Successfully deleted.',
                 'errorcode': 0,
-                'total' : order.total
+                'total': order.total
             }
-        return JsonResponse(response_data)
-    except Http404:
+            return JsonResponse(response_data)
+        except Http404:
+            response_data = {
+                'message': 'No record found.',
+                'errorcode': 1,
+                'total': 0
+            }
+            return JsonResponse(response_data)
+    else:
         response_data = {
-            'message': 'Order is not found.',
+            'message': 'Invalid request method.',
             'errorcode': 1,
-            'total' : 0
+            'total': 0
         }
         return JsonResponse(response_data)
 
+
 # Change quantity
-def change_quantity(request, pk):
-    pass
+def change_quantity(request):
+    if request.method == 'PUT':
+        try:
+            data = json.loads(request.body)
+            order_id = data.get('order_id')
+            order_item_id = data.get('order_item_id')
+            # Get actiom
+            action = data.get('action')
+
+            #Get order and order item
+            order = get_object_or_404(Order, orderId=order_id)
+            order_item = get_object_or_404(OrderItem, id=order_item_id, order=order)
+
+            if order_item.quantity > 0:
+                if action == 'plus':
+                    order_item.quantity += 1
+                else:
+                    order_item.quantity -= 1
+                order_item.save()
+                update_total(order)
+                response_data = {
+                    'message': '',
+                    'errorcode': 0,
+                    'total': order.total
+                }
+                return JsonResponse(response_data)
+            else:
+                response_data = {
+                    'message': 'Quantity cannot be negative',
+                    'errorcode': 0,
+                    'total': order.total
+                }
+                return JsonResponse(response_data)
+        except Http404:
+            response_data = {
+                'message': 'No record found.',
+                'errorcode': 1,
+                'total': 0
+            }
+            return JsonResponse(response_data)
+    else:
+        response_data = {
+            'message': 'Invalid request method.',
+            'errorcode': 1,
+            'total': 0
+        }
+        return JsonResponse(response_data)
