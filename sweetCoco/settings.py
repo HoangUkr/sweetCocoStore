92e4fd8b9286d2a83e89e8c14e80e75a1fe8c856
Pham Hoang <pxhoang@eccentex.com>
2024-05-21 17:21:25 +0200

    Edit setting

--- sweetCoco/settings.py
+++ sweetCoco/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_oa#6m(q&+rjioud@kf$k=bfn&a36s-a2hkf7@yp@!6_r45-3a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
