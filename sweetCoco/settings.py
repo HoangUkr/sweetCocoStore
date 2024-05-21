b5426ba8dd53e9b7d5a96642dc6a4af4f170b00a
Pham Hoang <pxhoang@eccentex.com>
2024-05-21 17:17:19 +0200

    Edit setting

--- sweetCoco/settings.py
+++ sweetCoco/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_oa#6m(q&+rjioud@kf$k=bfn&a36s-a2hkf7@yp@!6_r45-3a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
